d30a1adb9edac3d46b4bfce66a2d87e8745b981d
Curtis <curtis.laycraft@zoho.com>
2019-08-12 16:23:59 -0700

    externalize base64 encoder box

--- vsd-app/ClientApp/src/app/file-uploader-box/file-uploader-box.component.ts
+++ vsd-app/ClientApp/src/app/file-uploader-box/file-uploader-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-file-uploader-box',
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploaderBoxComponent implements OnInit {
 
+  @ViewChild('files')
+  myInputVariable: ElementRef;
+
+  // base64 encoded file turned into a string
+  fileNameList = [];
+  fileList = [];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  fakeBrowseClick() {
+    // the UI element for the native element is completely useless and ugly so we hide it and fake the user click.
+    this.myInputVariable.nativeElement.click();
+    console.log('Native button is clicked.');
+  }
+
+  onFilesAdded(files: FileList) {
+    // for each file added we go through the same conversion process.
+    for (let i = 0; i < files.length; i++) {
+      // convert the file to base64 for upload
+      const reader: FileReader = new FileReader();
+      reader.readAsDataURL(files.item(i));
+      reader.onload = () => {
+        if (this.fileNameList.indexOf(files.item(i).name) >= 0) {
+          // save the result over the old result
+          this.fileList[this.fileNameList.indexOf(files.item(i).name)] = reader.result;
+          console.log('Overwriting old file');
+        } else {
+          // push a fresh file name and file contents
+          this.fileNameList.push(files.item(i).name);
+          this.fileList.push(reader.result);
+          console.log('Adding new file');
+        }
+      };
+      reader.onerror = error => console.log('Error: ', error);
+
+    }
+    // this.resetFiles();
+  }
+  removeItem(index: number) {
+    console.log('Remove Item');
+    this.fileNameList.splice(index, 1);
+    this.fileList.splice(index, 1);
+  }
 }
