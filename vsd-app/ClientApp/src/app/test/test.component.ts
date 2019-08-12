ffaf65900c972e7082f9282462c5692e721510a4
Curtis <curtis.laycraft@zoho.com>
2019-08-12 13:12:59 -0700

    file selector

--- vsd-app/ClientApp/src/app/test/test.component.ts
+++ vsd-app/ClientApp/src/app/test/test.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  // base64 encoded file turned into a string
+  file = null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onFilesAdded(files: FileList) {
+    // convert the file to base64 for upload
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(files.item(0));
+    reader.onload = () => this.file = reader.result;
+    reader.onerror = error => console.log('Error: ', error);
+  }
 }
