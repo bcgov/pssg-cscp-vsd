a4318b00cc3c80c8d2a7a283a74c6ccf6ad41d8e
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-04-06 14:17:34 -0700

    Update config.groovy

--- jenkins/agent/config.groovy
+++ jenkins/agent/config.groovy
@@ -1,6 +1,6 @@
 // Pipeline Configuration Properties
 // Import this file into the pipeline using 'load'.
-class config extends bc.baseConfig {
+class config extends vsd.baseConfig {
   // Build configuration
   public static final String  APP_NAME = "agent"
   public static final String[] BUILDS = ["${this.APP_NAME}"]
