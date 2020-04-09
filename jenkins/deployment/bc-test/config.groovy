1e0ecd9b9ef78fdd4deed3b609cdb507d87a68e8
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-04-09 12:46:50 -0700

    Update config.groovy

--- jenkins/deployment/bc-test/config.groovy
+++ jenkins/deployment/bc-test/config.groovy
@@ -2,10 +2,8 @@
 // Import this file into the pipeline using 'load'.
 class config extends bc.baseConfig {
   // Deployment configuration
-  echo "in bc-test class"
   public static final String SOURCE_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[0]}"
   public static final String DESTINATION_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[1]}"
 }
 
-echo "about to return something"
 return new config();
