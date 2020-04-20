44e17503114ed6ac84c519db18bde249586cdca1
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-04-20 16:27:53 -0700

    Update config.groovy

--- jenkins/deployment/bc-test/config.groovy
+++ jenkins/deployment/bc-test/config.groovy
@@ -3,9 +3,7 @@
 class config extends bc.baseConfig {
   // Deployment configuration
   public static final String SOURCE_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[0]}"
-  echo SOURCE_TAG ;
   public static final String DESTINATION_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[1]}"
-  echo DESTINATION_TAG;
 }
 
 return new config();
