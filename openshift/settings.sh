bc1493a0df50bf1482ae7575335b2caeb98adccb
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-05-11 10:49:12 -0700

    Update settings.sh

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -11,10 +11,10 @@ export GIT_REF="development"
 
 # The project components
 # - defaults to the support the Simple Project Structure
-export components="vsd-app weasyprint"
+export components="vsd weasyprint"
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
-export builds=""
+export builds="vsd"
 
 # The images to be tagged after build
 export images="vsd"
