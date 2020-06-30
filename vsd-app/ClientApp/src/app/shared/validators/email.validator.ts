62afc5d69a47815ff4c5cff0a8930aea497fbb80
Joseph Weinkam <jweinkam@filmrobot.com>
2020-06-30 11:25:36 -0700

    fix attempt for abstractcontrol -> formcontrol issue

--- vsd-app/ClientApp/src/app/shared/validators/email.validator.ts
+++ vsd-app/ClientApp/src/app/shared/validators/email.validator.ts
@@ -1,30 +1,30 @@
-import { FormControl, AbstractControl } from '@angular/forms';
+// import { FormControl, AbstractControl } from '@angular/forms';
 
-export function EmailValidator(confirmEmailInput: string) {
-  let confirmEmailControl: AbstractControl;
-  let emailControl: AbstractControl;
+// export function EmailValidator(confirmEmailInput: string) {
+//   let confirmEmailControl: AbstractControl;
+//   let emailControl: AbstractControl;
 
-  return (control: AbstractControl) => {
-    if (!control.parent) {
-      return null;
-    }
+//   return (control: AbstractControl) => {
+//     if (!control.parent) {
+//       return null;
+//     }
 
-    if (!confirmEmailControl) {
-      confirmEmailControl = control;
-      emailControl = control.parent.get(confirmEmailInput) as AbstractControl;
-      emailControl.valueChanges.subscribe(() => {
-        confirmEmailControl.updateValueAndValidity();
-      });
-    }
+//     if (!confirmEmailControl) {
+//       confirmEmailControl = control;
+//       emailControl = control.parent.get(confirmEmailInput) as AbstractControl;
+//       emailControl.valueChanges.subscribe(() => {
+//         confirmEmailControl.updateValueAndValidity();
+//       });
+//     }
 
-    if (
-      emailControl.value.toLocaleLowerCase() !==
-      confirmEmailControl.value.toLocaleLowerCase()
-    ) {
-      return {
-        notMatch: true
-      };
-    }
-    return null;
-  };
-}
+//     if (
+//       emailControl.value.toLocaleLowerCase() !==
+//       confirmEmailControl.value.toLocaleLowerCase()
+//     ) {
+//       return {
+//         notMatch: true
+//       };
+//     }
+//     return null;
+//   };
+// }
