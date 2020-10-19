c163364a007fb84a16fe0146aa894f25bfeefe53
Joseph Weinkam <jweinkam@filmrobot.com>
2020-10-19 12:04:22 -0700

    remove some unnecessary comments

--- vsd-app/Services/TokenHandler.cs
+++ vsd-app/Services/TokenHandler.cs
@@ -12,19 +12,13 @@ namespace Gov.Cscp.VictimServices.Public.Services
         public TokenHandler(
             ICOASTAuthService coastAuthService)
         {
-            _coastAuthService = coastAuthService; ;
+            _coastAuthService = coastAuthService;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            // request the access token
             var accessToken = await _coastAuthService.GetToken();
-
-            // set the bearer token to the outgoing request
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            // Proceed calling the inner handler, that will actually send the request
-            // to our protected api
             return await base.SendAsync(request, cancellationToken);
         }
     }
