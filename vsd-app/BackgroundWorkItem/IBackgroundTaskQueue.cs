7866cc62e88a6f02034d1a7f3a5c66e958b00d36
Sam Warren <samwarren4@gmail.com>
2024-07-30 16:20:59 -0600

    VS-6867: Commit schedule job changes

---
+++ vsd-app/BackgroundWorkItem/IBackgroundTaskQueue.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace Gov.Cscp.VictimServices.Public.BackgroundWorkItem
+{
+    /// <summary>
+    /// Background Task Queue
+    /// </summary>
+    public interface IBackgroundTaskQueue
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<Func<CancellationToken, Task>?> DequeueAsync(CancellationToken cancellationToken);
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="workItem"></param>
+        void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem);
+    }
+}
---
+++ vsd-app/Controllers/BackgroundTaskController.cs
@@ -0,0 +1,166 @@
+using Gov.Cscp.VictimServices.Public.Models;
+using Gov.Cscp.VictimServices.Public.Services;
+using Gov.Cscp.VictimServices.Public.BackgroundWorkItem;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace Gov.Cscp.VictimServices.Public.Controllers
+{
+    public class Incident
+    {
+        public string TicketNumber { get; set; }
+        public Guid IncidentId { get; set; }
+        public int StatusCode { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class BackgroundTaskController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly IDynamicsResultService _dynamicsResultService;
+        private readonly ILogger _logger;
+        private readonly IBackgroundTaskQueue _backgroundWorkerQueue;
+
+
+        public BackgroundTaskController(IConfiguration configuration, IDynamicsResultService dynamicsResultService, IBackgroundTaskQueue backgroundWorkerQueue)
+        {
+            this.configuration = configuration;
+            this._dynamicsResultService = dynamicsResultService;
+            _logger = Log.Logger;
+            _backgroundWorkerQueue = backgroundWorkerQueue;
+        }
+
+        private async Task<bool> IsCaseInactive(Guid incidentId)
+        {
+            try
+            {
+                string endpointAction = $"incidents({incidentId})/Microsoft.Dynamics.CRM.vsd_IsCaseInactive";
+                DynamicsResult result = await _dynamicsResultService.Post(endpointAction, "{}");
+                if (result.statusCode != HttpStatusCode.OK)
+                {
+                    _logger.Error($"Failed to check if case is inactive. Status code: {result.statusCode}");
+                    return false;
+                }
+                // Log the result
+                _logger.Information($"IsCaseInactive result: {result.result}");
+                return result.result["IsInactive"].ToObject<bool>();
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Error in IsCaseInactive");
+                return false;
+            }
+        }
+
+        [HttpPost("CaseClosure")]
+        public async Task<IActionResult> CaseClosure()
+        {
+            // _backgroundWorkerQueue.QueueBackgroundWorkItem(async token => 
+            // {
+                try {
+                    const string fetchXml = @"
+                    <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                        <entity name='incident'>
+                            <attribute name='ticketnumber'/>
+                            <attribute name='incidentid'/>
+                            <attribute name='statuscode'/>
+                            <order attribute='ticketnumber' descending='false'/>
+                            <filter type='and'>
+                            <condition attribute='vsd_programunit' operator='eq' value='100000000'/>
+                            <condition attribute='ticketnumber' operator='not-like' value='VSD%'/>
+                            <condition attribute='statuscode' operator='in'>
+                                <value>1</value>
+                                <value>4</value>
+                                <value>3</value>
+                            </condition>
+                            <condition attribute='createdon' operator='olderthan-x-months' value='6'/>
+                            <condition attribute='vsd_cvap_pensionfile' operator='ne' value='1'/>
+                            <condition attribute='vsd_cvap_entitlements' operator='in'>
+                                <value>100000006</value>
+                                <value>100000007</value>
+                            </condition>
+                            </filter>
+                        </entity>
+                    </fetch>";
+
+                    string fetchEndpointUrl = $"incidents?fetchXml={Uri.EscapeDataString(fetchXml)}";
+
+                    DynamicsResult result = await _dynamicsResultService.Get(fetchEndpointUrl);
+
+                    _logger.Information("Result from Dynamics: " + result.result);
+
+                    if (result.statusCode != HttpStatusCode.OK)
+                    {
+                        _logger.Error($"Failed to retrieve incidents. Status code: {result.statusCode}");
+                        Serilog.Log.Error("Failed to retrieve incidents.");
+                    }
+
+                    var incidents = result.result["value"].ToObject<List<Incident>>();
+                    if (incidents == null || incidents.Count == 0)
+                    {
+                        _logger.Information("No incidents found for the given criteria.");
+                        Serilog.Log.Error("No incidents found for the given criteria.");
+                    }
+                    
+                    // Log the GUIDs of the fetched incidents
+                    _logger.Information($"Incident GUIDs: {JsonSerializer.Serialize(incidents)}");
+
+                    // Iterate through each fetched incident
+                    foreach (var incident in incidents)
+                    {
+                    // log the incident details
+                    _logger.Information($"Incident details: {JsonSerializer.Serialize(incident)}");
+                    
+                    // Check if the case is inactive
+                    bool isCaseInactive = await IsCaseInactive(incident.IncidentId);
+
+                    // Log the value of the incident id with the returned value
+                    _logger.Information($"IncidentId: {incident.IncidentId}, IsCaseInactive: {isCaseInactive}");
+
+                    /* statuscode:
+                        - 100,000,001: Draft
+                        - 100,000,004: Processing
+                        - 2: On Hold
+                        - 100,000,003: New
+                        - 1: In Progress
+                        - 4: Waiting for Details
+                        - 3: Idle
+                    */
+
+                    if (isCaseInactive) 
+                    {
+                        if (incident.StatusCode == 1)
+                        {
+                            // Update case:
+                            // Update Claim Progress Status to "Closed"
+                            // Update Case Conclusion Result to "Case is auto closed due to inactivity from 6 months."
+                            // Update Case Resolution to "Fulfilled/Concluded"
+                            // Update Resolve Case to "Yes"
+                        }
+                        else if (incident.StatusCode == 3 || incident.StatusCode == 4 || incident.StatusCode == 100000003) 
+                        {
+                            // Change record status to 1 (In Progress)
+                            // Update case: 
+                            // Update Claim Progress Status to "Closed"
+                            // Update Case Conclusion Result to "Case is auto closed due to inactivity from 6 months."
+                            // Update Case Resolution to "Fulfilled/Concluded"
+                            // Update Resolve Case to "Yes"
+
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                _logger.Error(e, "Error in CaseClosure");
+                Serilog.Log.Error(e, "Error in CaseClosure");
+            }
+        // }); 
+        return Ok();
+        }
+    }
+}
