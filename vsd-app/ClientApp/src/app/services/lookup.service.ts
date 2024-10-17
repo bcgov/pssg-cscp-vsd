cca7608384c5a32184d63ccde0c6ee924989295a
AJ Po-Déziel <alainjoseph.podeziel@gov.bc.ca>
2024-10-17 15:45:27 -0700

    Add base path to API calls

--- vsd-app/ClientApp/src/app/services/lookup.service.ts
+++ vsd-app/ClientApp/src/app/services/lookup.service.ts
@@ -2,6 +2,7 @@ import { retry, catchError } from 'rxjs/operators';
 import { Observable, throwError, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -9,7 +10,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class LookupService {
   // this should query the test api
-  apiUrl = 'api/Lookup';
+  baseUrl = environment.apiRootUrl;
+  apiPath = this.baseUrl.concat('api/Lookup');
 
   cvapEmail: string = "";
   cvapCounsellingEmail: string = "";
@@ -19,28 +21,28 @@ export class LookupService {
   ) { }
 
   getCVAPEmails(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cvap-emails`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/cvap-emails`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getCountries(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/countries`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/countries`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getProvinces(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/provinces`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/provinces`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getCities(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cities`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/cities`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
@@ -48,7 +50,7 @@ export class LookupService {
 
   searchCities(country: string, province: string, searchVal: string): Observable<any> {
     let limit = 15;
-    return this.http.get<any>(`${this.apiUrl}/cities/search?country=${country}&province=${province}&searchVal=${searchVal}&limit=${limit}`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/cities/search?country=${country}&province=${province}&searchVal=${searchVal}&limit=${limit}`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
@@ -56,56 +58,56 @@ export class LookupService {
 
 
   getCitiesByCountry(country_id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/country/${country_id}/cities`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/country/${country_id}/cities`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getCitiesByProvince(country_id: string, province_id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/country/${country_id}/province/${province_id}/cities`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/country/${country_id}/province/${province_id}/cities`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getRelationships(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/relationships`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/relationships`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getOptionalAuthorizationRelationships(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/auth_relationships`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/auth_relationships`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getRepresentativeRelationships(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/representative_relationships`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/representative_relationships`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getRestitutionRelationships(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/restitution_relationships`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/restitution_relationships`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getPoliceDetachments(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/police_detachments`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/police_detachments`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
   }
 
   getCourts(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/courts`, { headers: this.headers }).pipe(
+    return this.http.get<any>(`${this.apiPath}/courts`, { headers: this.headers }).pipe(
       retry(3),
       catchError(this.handleError)
     );
