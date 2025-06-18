1e58e8a923370e12fc9c47e04223945aa0c4030d
Fintan Roche <Fintan.Roche@quartech.com>
2025-06-17 23:39:25 -0700

    Map relationship "Third party applying on behalf of an incapable adult" to onbehalfof "third Party"

---
+++ vsd-app/Models/OnBehalfOfEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gov.Cscp.VictimServices.Public.Models
+{
+    public enum OnBehalfOfEnum
+    {
+        ThirdParty = 100000001,
+        Self = 100000002,
+        LegalRepGuardian = 100000003
+ 
+        
+    }
+}
