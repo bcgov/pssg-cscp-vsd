b99a94637da258c8593b64d197c2bea63996a0b3
Vlad Belukha <vlad.belukha@quartech.com>
2026-03-10 17:09:38 -0700

    Create application and invoice buttons for dashboard navigation

--- vsd-app/ClientApp/src/app/app-routing.module.ts
+++ vsd-app/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApplicationCancelledComponent } from './application-cancelled/application-cancelled.component';
+import { ApplicationSelectorComponent } from './application-selector/application-selector.component';
 import { ApplicationSuccessComponent } from './application-success/application-success.component';
 import { DraftDashboardComponent } from './draft-dashboard/draft-dashboard.component';
 import { authGuard } from './guards/auth.guard';
-import { ApplicationSelectorComponent } from './application-selector/application-selector.component';
 import { IfmApplicationComponent } from './ifm-application/ifm-application.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
